cd68d541544d1cdb92f2dc38940c1c69a76e992b
Andrea Perego <andrea-perego@users.noreply.github.com>
2015-05-12 23:57:54 +0200

    Added function to normalise the WKT string, by removing unnecessary white space. The original URL encoding procedure for the WKT string has been moved to a specific function.

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,6 +1,15 @@
 function getGeoIRI() {
   var srid = document.getElementById("srid").value;
-  var geometryWKT = document.getElementById("geometry-wkt").value.replace(/\s/g, '_').replace(/\+/g, '%2B');
+//  var geometryWKT = document.getElementById("geometry-wkt").value.replace(/\s/g, '_').replace(/\+/g, '%2B');
+  var geometryWKT = encodeWKT(normaliseWKT(document.getElementById("geometry-wkt").value));
   var pathPrefix = document.getElementById("geoiri").action;
   window.location.href = pathPrefix + '/id/geometry/' + srid + '/' + geometryWKT;
-}    
+}
+// Removes unnecessary white spaces from the WKT string.
+function normaliseWKT(wkt) {
+  return wkt.trim().replace(/[\s]*([\(\),])[\s]*/g, '$1').replace(/[\s]+/g, ' ');
+}
+// Encodes the WKT string to be used in the URI path.
+function encodeWKT(wkt) {
+  return wkt.replace(/\s/g, '_').replace(/\+/g, '%2B');
+}
